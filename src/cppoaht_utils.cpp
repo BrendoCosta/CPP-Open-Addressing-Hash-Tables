119fd3d4d47bb99d037778c8d14b93db9d040796
Brendo Costa <brendocosta@id.uff.br>
2021-10-11 20:30:01 -0300

    First functions implementations

---
+++ src/cppoaht_utils.cpp
@@ -0,0 +1,76 @@
+/*
+    cppoaht_quadratic.cpp
+    
+    TODO
+    
+    ------------------------------------------------------------------------------
+    
+    MIT License
+    
+    Copyright (c) 2021 Brendo Costa
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    
+*/
+
+#include "src/include/cppoaht_utils.h"
+
+namespace CPPOAHT {
+    
+    template <typename integer_type>
+    bool isPrime(integer_type number) {
+        
+        if (number == 2) return true;
+            
+        if (number == 1 || number % 2 == 0) return false;
+        
+        for (integer_type i = 3; (i * i) < number + 1; i += 2) {
+            
+            if (number % i == 0) return false;
+            
+        }
+        
+        return true;
+        
+    }
+    
+    /* --- */
+    
+    template <typename integer_type>
+    integer_type nextPrime(integer_type number) {
+        
+        if (number <= 1) {
+                
+                return 2;
+                
+        } else {
+            
+            while (!isPrime(number)) {
+                
+                number++;
+                
+            }
+            
+            return number;
+            
+        }
+    
+    }
+    
+}
